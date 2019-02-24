ae97d2037ce03d33678bc6184fae82815f70430e
sean <31554433+siriusfinex@users.noreply.github.com>
2019-02-24 23:13:15 +0800

    Update bash-script-1.sh

--- bash-script-1.sh
+++ bash-script-1.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
 #write ulimit value to supervisor
-echo "
-ulimit -n 51200" >> /etc/default/supervisor &&
+echo "ulimit -n 51200" >> /etc/default/supervisor &&
 service stop supervisor && service start supervisor
 
 #fix ssmgr-tiny restart rule in crontab
 sed -i 's#reload#restart\ ssmgt-tiny#g' /etc/crontab
 
 #setting server reboot time
-echo "
-5 5     25 2 1  root    reboot" >> /etc/crontab &&
+echo "5 5     25 2 1  root    reboot" >> /etc/crontab &&
 service cron reload
