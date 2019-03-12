08cf0c5224e9b8302d0c17ada292072d67c955cb
sean <31554433+siriusfinex@users.noreply.github.com>
2019-03-12 14:38:02 +0800

    bug fix

--- install-shadowsocks_libev-from-latest-version.sh
+++ install-shadowsocks_libev-from-latest-version.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 #
 #make git directory
-if [ ! -d "~/git" ]; then
-	mkdir ~/git
+if [ ! -d "/root/git" ]; then
+        mkdir /root/git
+else
+        echo "ok"
 fi
-if [ ! -d "~/git/shadowsocks-libev" ]; then
-	mkdir ~/git/shadowsocks-libev
+if [ ! -d "/root/git/shadowsocks-libev" ]; then
+        mkdir /root/git/shadowsocks-libev
+else
+        echo "ok"
+fi
+if [ ! -d "/root/build-area" ]; then
+        echo "ok"
+else
+        rm -rf /root/build-area
 fi
 
 #install dependencies
@@ -14,13 +23,12 @@ apt install -y gawk debhelper dh-systemd init-system-helpers pkg-config git
 
 #download and install shadowsocks-libev
 cd ~/git/shadowsocks-libev
-ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases/latest | grep 'tag_name' | cut -d\" -f4)
-[ -z ${ver} ] && echo "Error: Get shadowsocks-libev latest version failed" && exit 1
+ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases/latest | grep 'tag_name' | cut -d\" -f4); [ -z ${ver} ] && echo "Error: Get shadowsocks-libev latest version failed" && exit 1
 shadowsocks_libev_ver="shadowsocks-libev-$(echo ${ver} | sed -e 's/^[a-zA-Z]//g')"
 download_link="https://github.com/shadowsocks/shadowsocks-libev/releases/download/${ver}/${shadowsocks_libev_ver}.tar.gz"
 wget -qO "${shadowsocks_libev_ver}.tar.gz" "${download_link}"
 tar zxf ${shadowsocks_libev_ver}.tar.gz
 mkdir -p ~/build-area/
-cp ./scripts/build_deb.sh ~/build-area/
+cp ./${shadowsocks_libev_ver}/scripts/build_deb.sh ~/build-area/
 cd ~/build-area
 ./build_deb.sh
